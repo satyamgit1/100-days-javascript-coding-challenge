dc578cfe67ecbe4d4b404f98df7bf6a3740e7820
Satyam singh <satyamsingh2003a@gmail.com>
2024-04-18 19:23:35 +0530

    day 23 solved

---
+++ Day23.js
@@ -0,0 +1,17 @@
+// ounts the occurrences of each element in an array
+
+function countOccurrences(numbers) {
+    const counts = {}; // Create an empty object to store the counts of each element
+    
+    // Iterate through each number in the array
+    for (let element of numbers) {
+      counts[element] = (counts[element] || 0) + 1;
+    }
+  
+    // Return the counts object containing the counts of each element
+    return counts;
+  }
+  
+  const numbers = [1, 2, 2, 3, 1, 4, 2];
+  console.log(countOccurrences(numbers));
+  
