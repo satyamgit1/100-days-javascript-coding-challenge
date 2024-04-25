17e016feee680756248ae82ada474a8a7c115e92
Satyam singh <satyamsingh2003a@gmail.com>
2024-04-25 20:22:01 +0530

    solved 30 days question

--- Day30.js
+++ Day30.js
@@ -14,9 +14,10 @@
 const numberRangeRecursive = (a, b,arr=[]) =>{
    if(a<=b){
     arr.push(a);
-    
+
 }
   
+}
   console.log(numberRangeRecursive (0, 5)); // Output: [0, 1, 2, 3, 4, 5]
   console.log(numberRangeRecursive (-2, 2)); // Output: [ -2, -1, 0, 1, 2 ]
   
