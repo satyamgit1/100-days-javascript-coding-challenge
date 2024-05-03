fead97305db21fd2b9195ff0f91438184a83d5cf
Satyam singh <satyamsingh2003a@gmail.com>
2024-05-03 20:49:55 +0530

    day 37 solved

---
+++ Day37.js
@@ -0,0 +1,13 @@
+
+1 //*-
+3 //*
+2 //* Programming Challenge: Simple Interest Calculator
+4
+5 //? Write a function calculateSimpleInterest that calculates the simple interest given the principal amount, rate of interest per annum, and time in years.
+const calculateSimpleInterest = (p,t,r) => {
+    return p * r *  t/100;
+}
+
+
+
+console.log(calculateSimpleInterest (1000, 5, 3)); // Output: 150
--- pratice.js
+++ pratice.js
@@ -17,28 +17,61 @@
 
 
 // Q.2 ) Longest common prefix
-function longestCommonPrefix(strs) {
-    if (strs.length === 0) return ""; // Return empty string if the array is empty
+// function longestCommonPrefix(strs) {
+//     if (strs.length === 0) return ""; // Return empty string if the array is empty
     
-    // Sort the array of strings
-    strs.sort();
-    let prefix = "";
-    const first = strs[0];
-    const last = strs[strs.length - 1];
+//     // Sort the array of strings
+//     strs.sort();
+//     let prefix = "";
+//     const first = strs[0];
+//     const last = strs[strs.length - 1];
 
-    // Iterate through characters of the first and last strings
-    for (let i = 0; i < first.length; i++) {
-        if (first[i] === last[i]) {
-            prefix += first[i]; // Append the character to the prefix if it matches in both strings
-        } else {
-            break; // Break the loop if the characters don't match
-        }
-    }p
-    return prefix;
-}
+//     // Iterate through characters of the first and last strings
+//     for (let i = 0; i < first.length; i++) {
+//         if (first[i] === last[i]) {
+//             prefix += first[i]; // Append the character to the prefix if it matches in both strings
+//         } else {
+//             break; // Break the loop if the characters don't match
+//         }
+//     }p
+//     return prefix;
+// }
 
-// Example usage:
-const strs = ["flower", "flow", "flight"];
-console.log(longestCommonPrefix(strs));
+// // Example usage:
+// const strs = ["flower", "flow", "flight"];
+// console.log(longestCommonPrefix(strs));
 
+
+// function findMissingNumber(nums) {
+//     nums.sort((a, b) => a - b);
+//     console.log(nums)
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== i) {
+//             return i;
+//         }
+//     }
+//     return nums.length;
+// }
+
+// // Example usage:
+// const nums = [5, 4, 3, 1, 6];
+// console.log("The missing number is:", findMissingNumber(nums));
+
+// javascript
+// Copy code
+// var missingNumber = function(nums) {
+//     let res = nums.length;
+// Here, res is initialized to the length of the nums array, which is 3 in our example.
+
+// javascript
+// Copy code
+//     for (let i = 0; i < nums.length; i++) {
+//         res += i - nums[i];
+//     }
+// The loop iterates over each index i from 0 to nums.length - 1. In each iteration, it calculates i - nums[i]. For example:
+
+// At i = 0, i - nums[i] is 0 - 3, which equals -3.
+// At i = 1, i - nums[i] is 1 - 0, which equals 1.
+// At i = 2, i - nums[i] is 2 - 1, which equals 1.
+// Then, it adds each of these differences to res. So, res becomes 3 - 3 + 1 + 1, which equals 2.
 
