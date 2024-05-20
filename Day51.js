e032eefb92edf2dda539e4aab2cd03870ea098a6
Satyam singh <satyamsingh2003a@gmail.com>
2024-05-20 22:37:45 +0530

    solved day 50

---
+++ Day51.js
@@ -0,0 +1,15 @@
+function findLongestWord(sentence) {
+    const words = sentence.split(' ');
+    let longestWord = '';
+  
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > longestWord.length) {
+        longestWord = words[i];
+      }
+    }
+  
+    return longestWord;
+  }
+  
+  console.log(findLongestWord('The quick brown fox jumps over the lazy dog'));
+  // Output: 'quick'
