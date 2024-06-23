ade25818500c82dbd2ee0fbe969a6434aa34eab5
Satyam singh <satyamsingh2003a@gmail.com>
2024-06-23 21:49:19 +0530

    solved day 77

---
+++ Day77.js
@@ -0,0 +1,5 @@
+// Average Salary Excluding Minimum and Maximum
+// Easy
+// 15
+// 69.3% Acceptance
+// In this lab, you will be implementing a function to calculate the average salary of employees excluding the minimum and maximum salary. You are given an array of unique integers salary where salary[i] is the salary of the ith employee. Your task is to return the average salary of employees excluding the minimum and maximum salary. The answers within 10-5 of the actual answer will be accepted.
